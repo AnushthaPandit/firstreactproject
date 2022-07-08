eba5ef3cb169dce939f27293c058ea53e5faa26d
Sandeep Gupta <sandeepguptabc@gmail.com>
2022-07-08 14:47:37 +0530

    routes and typo fixes

--- src/ViewPage.jsx
+++ src/ViewPage.jsx
@@ -1,15 +1,11 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
 
 const ViewPage = () => {
-    return (
-        <>
-        <Navbar />
-        <Typography variant="h3">This is View page!!!</Typography>
-        <Footer />
-        </>
-    );
+	return (
+		<>
+			<Typography variant="h3">This is View page!!!</Typography>
+		</>
+	);
 };
 export default ViewPage;
